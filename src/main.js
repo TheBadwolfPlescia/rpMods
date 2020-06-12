2e67b49ab6b206e36f130b0505d416ca05f1bd5e
Anthony Plescia <aplescia@ucsc.edu>
2020-06-11 22:44:57 -0700

    rnd

--- src/main.js
+++ src/main.js
@@ -24,7 +24,7 @@ let game = new Phaser.Game(config);
 
 // define game settings
 game.settings = {
-    spaceshipSpeed: Phaser.Math.Between(-1, 1),
+    spaceshipSpeed: Phaser.Math.Between(-5, 5),
     gameTimer: 60000,    
 }
 
