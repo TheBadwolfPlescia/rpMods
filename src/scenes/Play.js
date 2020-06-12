73c9bfed2d8c575faa60ee209c4edaac3e3c97c1
Anthony Plescia <aplescia@ucsc.edu>
2020-06-11 20:34:44 -0700

    hs

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -70,7 +70,7 @@ class Play extends Phaser.Scene {
         // score
         this.p1Score = 0;
 
-        this.highScore = 0;
+        this.highScore = this.highScore;
 
         // score display
         let scoreConfig = {
